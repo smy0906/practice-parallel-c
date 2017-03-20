d4640a82556962ea9e1f185423c874fcdbb3cae9
sungminyang <smy0906@gmail.com>
2017-03-20 14:48:29 +0900

    edit test.sh

--- CompareAndSwap/test.sh
+++ CompareAndSwap/test.sh
@@ -6,11 +6,12 @@
 #  Created by Yang on 2017. 3. 20..
 #
 
-git pull
+#git pull
 gcc -I/opt/papi/5.5.1/include -lpthread -O3 main.c /opt/papi/5.5.1/lib/libpapi.a -o main
 I_NUM=10000000
-for T_NUM in 2 4 6 8 16
+for T_NUM in 2 4 8 16 32
 do
+    printf '\n==============\nT_NUM = %d\n==============\n' $T_NUM
     ./main --i $I_NUM --t $T_NUM
-    printf '\n\n'
+    printf '\n'
 done
