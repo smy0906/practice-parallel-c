d44a9ed23a52e54bd4ad41aff1542afdf7336979
sungminyang <smy0906@gmail.com>
2017-03-20 14:25:06 +0900

    edit test.sh

--- CompareAndSwap/test.sh
+++ CompareAndSwap/test.sh
@@ -9,10 +9,8 @@
 git pull
 gcc -I/opt/papi/5.5.1/include -lpthread -O3 main.c /opt/papi/5.5.1/lib/libpapi.a -o main
 I_NUM=10000000
-T_NUM=2
-while [ $T_NUM -lt 17 ]
+for T_NUM in 2 4 6 8 16
 do
-    ./main --i $I --t $T_NUM
-    echo '\n\n'
-    $T_NUM=$T_NUM-1
+    ./main --i $I_NUM --t $T_NUM
+    printf '\n\n'
 done
